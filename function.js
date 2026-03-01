46bf5cda05a8166b37fa5496b493fa0bd919f973
Mahmudullah <153950664+mahmudullahm70@users.noreply.github.com>
2026-03-01 19:35:30 +0600

    Add initial function.js with variable declarations

---
+++ function.js
@@ -0,0 +1,32 @@
+
+// My Nam is "Sajib"
+let person1="Sajib";
+let person2="Dalim";
+let myString=`Welcome ${person1} and ${person2}`;
+
+let a=50.20;
+let b=60;
+
+let sum=3/0;
+
+let myTest=450359962737049666n
+let myTest2=450359962737049666n
+
+let myValue;
+
+
+
+console.log(typeof myValue)
+
+let person={
+    name:"Chomok",
+    age:20,
+    height:`5.10"`,
+    weight:"65Kg",
+    fatherName:"Mr X",
+    motherName:"Mr Y",
+    isMarrid:false
+}
+
+let tst=(10+100)*5;
+console.log(tst)
