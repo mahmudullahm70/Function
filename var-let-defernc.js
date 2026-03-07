eab7e88cc046114ba778bfd1a9372161680a4aba
Mahmudullah <153950664+mahmudullahm70@users.noreply.github.com>
2026-03-08 04:34:53 +0600

    Introduce variable scoping examples in var-let-defernc.js
    Add example demonstrating variable scoping with var, let, and const.

---
+++ var-let-defernc.js
@@ -0,0 +1,21 @@
+// if(true){
+//     var x=40;
+
+// }
+// console.log(x);
+
+
+// //let deference 
+
+// if (true){
+//    let y=59;
+   
+// }
+// console.log(y);
+
+  const car ={
+       name:"tasla",
+       model:56,
+       price:3999,
+  }
+  console.log(car.name);
